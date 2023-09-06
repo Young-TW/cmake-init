f141e5015374c0a1740bb7a2e43c3428ceb5ae6e
young <young20050727@gmail.com>
2023-09-07 01:32:45 +0800

    update cmake version

--- include/files/CMakeLists_txt.hpp
+++ include/files/CMakeLists_txt.hpp
@@ -7,7 +7,7 @@
 
 int cmakelists_txt(std::string project_name, int cxx_std){
     fout.open("CMakeLists.txt");
-    fout << "cmake_minimum_required(VERSION 3.0.0)\n"
+    fout << "cmake_minimum_required(VERSION 3.20.0)\n"
     << "project(" << project_name << " VERSION 0.0.0)\n\n"
     << "set(CMAKE_CXX_STANDARD " << cxx_std << ")\n\n"
     << "include_directories(./include)\n\n"
