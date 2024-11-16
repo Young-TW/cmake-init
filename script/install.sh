31543475bb7fb50e49f43769b1feefdb5fa5b055
Young <young20050727@gmail.com>
2024-11-16 11:45:52 +0800

    script: update install.sh for installing using cargo

--- script/install.sh
+++ script/install.sh
@@ -6,30 +6,13 @@ if ! [ -x "$(command -v git)" ]; then
   exit 1
 fi
 
-# check cmake is installed
-if ! [ -x "$(command -v cmake)" ]; then
-  echo 'Error: cmake is not installed.' >&2
+# check cargo is installed
+if ! [ -x "$(command -v cargo)" ]; then
+  echo 'Error: cargo is not installed.' >&2
   exit 1
 fi
 
-# check ninja is installed
-if ! [ -x "$(command -v ninja)" ]; then
-  echo 'Error: ninja is not installed.' >&2
-  exit 1
-fi
-
-# check clang is installed
-if ! [ -x "$(command -v clang)" ]; then
-  echo 'Error: clang is not installed.' >&2
-  exit 1
-fi
-
-git clone --recursive https://github.com/Young-TW/cmake-init.git
+git clone https://github.com/Young-TW/cmake-init.git
 cd cmake-init || exit
-echo "start Build"
-git submodule update --init --recursive
-cmake -B build
-cmake --build build --config RELEASE
-sudo cmake --install build --config RELEASE
-echo "cmake-init installed successfully"
+cargo install --path .
 cd .. || exit
