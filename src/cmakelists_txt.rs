8055ca2bcc5a2c32dc6eb6bb98d72895431dbef6
Young <young20050727@gmail.com>
2025-06-16 17:02:29 +0800

    feat: add cuda and hip CMakeLists.txt

--- src/cmakelists_txt.rs
+++ src/cmakelists_txt.rs
@@ -1,9 +1,27 @@
 use std::fs::File;
 use std::io::Write;
 
-pub fn cmakelists_txt(project_name: &str, cxx_std: i32) {
+pub fn cmakelists_txt(project_name: &str, cxx_std: i32, mode: Option<&str>) {
     let mut file = File::create("CMakeLists.txt").unwrap();
-    let origin_content = include_str!("../files/CMakeLists.txt");
+    let origin_content;
+    match mode {
+        Some("CUDA") => {
+            origin_content = include_str!("../files/cuda/CMakeLists.txt");
+        }
+        Some("HIP") => {
+            origin_content = include_str!("../files/hip/CMakeLists.txt");
+        }
+        _ => {
+            origin_content = include_str!("../files/CMakeLists.txt");
+
+        }
+    }
+
+    // Check if the file already exists
+    if std::path::Path::new("CMakeLists.txt").exists() {
+        eprintln!("CMakeLists.txt already exists. Skipping creation.");
+        return;
+    }
     // Replace project_name and cxx_std in the content
     let projname_content = origin_content.replace("{{PROJECT_NAME}}", project_name);
     let content = projname_content.replace("{{cxx_std}}", &cxx_std.to_string());
@@ -17,7 +35,7 @@ mod tests {
     #[test]
     fn test_cmakelists_txt_project_name() {
         let project_name = "minesweeper";
-        cmakelists_txt(project_name, 17);
+        cmakelists_txt(project_name, 17, Some("C++"));
         let content = std::fs::read_to_string("CMakeLists.txt").unwrap();
         assert!(content.contains("project(minesweeper VERSION 0.0.0)"));
     }
--- src/main.rs
+++ src/main.rs
@@ -89,19 +89,19 @@ fn main() {
     match mode {
         "CUDA" => {
             src_main_cpp(Some("CUDA")); // External function for creating src/main.cpp
-            cmakelists_txt(project_name, cxx_std); // External function for creating CMakeLists.txt
+            cmakelists_txt(project_name, cxx_std, Some("CUDA")); // External function for creating CMakeLists.txt
             // Additional CUDA-specific setup can be added here
             println!("CUDA support enabled.");
         },
         "HIP" => {
             src_main_cpp(Some("HIP")); // External function for creating src/main.cpp
-            cmakelists_txt(project_name, cxx_std); // External function for creating CMakeLists.txt
+            cmakelists_txt(project_name, cxx_std, Some("HIP")); // External function for creating CMakeLists.txt
             // Additional HIP-specific setup can be added here
             println!("HIP support enabled.");
         },
         _ => {
              src_main_cpp(Some("C++")); // External function for creating src/main.cpp
-             cmakelists_txt(project_name, cxx_std); // External function for creating CMakeLists.txt
+             cmakelists_txt(project_name, cxx_std, Some("C++")); // External function for creating CMakeLists.txt
         },
     }
 
