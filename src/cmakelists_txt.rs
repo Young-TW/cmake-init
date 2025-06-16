9fd79bf3c351b2c45761717e11883385a3b8751e
Young <young20050727@gmail.com>
2025-06-16 17:19:52 +0800

    test: remove test of cmakelists.txt

--- src/cmakelists_txt.rs
+++ src/cmakelists_txt.rs
@@ -27,16 +27,3 @@ pub fn cmakelists_txt(project_name: &str, cxx_std: i32, mode: Option<&str>) {
     let content = projname_content.replace("{{cxx_std}}", &cxx_std.to_string());
     file.write_all(content.as_bytes()).unwrap();
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_cmakelists_txt_project_name() {
-        let project_name = "minesweeper";
-        cmakelists_txt(project_name, 17, Some("C++"));
-        let content = std::fs::read_to_string("CMakeLists.txt").unwrap();
-        assert!(content.contains("project(minesweeper VERSION 0.0.0)"));
-    }
-}
